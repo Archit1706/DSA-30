ee7fdd96faeadde5e5cd4fc8d33a837eadef9c05
Archit1706 <architrathod77@gmail.com>
2023-09-23 14:35:04 +0530

    Tailwind Configure

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js,jsx,md,mdx}", "./components/**/*.{html,js,jsx,md,mdx}", "./templates/**/*.{html,js,jsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
